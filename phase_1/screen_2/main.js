b047eb4300e429ec0d717933f5cc01716d0cbd67
Ilay Skutelsky <ilay.skutelsky@gmail.com>
2019-12-02 22:45:01 +0200

    screen 2 structure

--- phase_1/screen_2/main.js
+++ phase_1/screen_2/main.js
@@ -1,32 +1,59 @@
 let state = {
-  isWorking: false,
+  stage: 1,
   workingSinceTimestamp: null,
-  stoppedSinceTimestamp: Date.now(),
+  stoppedSinceTimestamp: null,
   stopReasonsJSON: null
 }
 
 function init(){
   getAndUpdateData();
-  getAndShowReasons();
-  // get reasons and render
+  getAndRenderReasons();
 }
 
 function getAndUpdateData() {
   getAndShowTime();
+  // getLineState();
   // updateDuration();
   setTimeout(getAndUpdateData, 10000);
 }
 
-function getAndShowReasons() {
+function startAdjusting() {
+  state.stage = 2
+  console.log("starting to adjust");
+}
+
+function startWorking() {
+  state.stage = 3
+  console.log("starting to work");
+}
+
+function stopWorking() {
+  state.stage = 4
+  console.log("stopped working");
+}
+
+
+function goBackToWork() {
+  state.stage = 3
+  console.log("back to working");
+}
+
+function reportReason(id) {
+  console.log("reporting reason: " + id);
+}
+
+function getAndRenderReasons() {
   // $(".prod-rate .lds-ellipsis").addClass("hidden")
   $.ajax({
      url: "http://www.chocolatepanda.co.il/kav/stop_reasons.php",
    })
   .done((result) => {
-    console.log(" >>> getAndShowReasons succes! result: " + result);
+    console.log(" >>> getAndShowReasons succes! result: ");
+    console.log(JSON.parse(result));
   })
-  .fail((error) => {
-    console.log(" >>> getAndShowReasons error!: " + JSON.stringify(error));
+  .fail((error) => {   $(".clock").text()
+
+    console.log(" >>> getAndShowReasons error!: " + JSON.parse(error));
   })
 
   let json = `[{"title":"break","id":0},{"title":"technical","id":1},{"title":"rnd of shift","id":2},{"title":"break","id":3},{"title":"technical","id":4},{"title":"rnd of shift","id":4},{"title":"break","id":6},{"title":"technical","id":7},{"title":"rnd of shift","id":8},{"title":"break","id":9},{"title":"technical","id":10},{"title":"rnd of shift","id":11}]`
@@ -39,34 +66,12 @@ function getAndShowReasons() {
 }
 
 
-function goBackToWork() {
-  $(".stop-button span").text(" ")
-  $(".stop-button .lds-ellipsis").removeClass("hidden");
-  $.ajax({
-     url: "http://www.chocolatepanda.co.il/kav/start_line.php",
-     method: "POST"
-   })
-  .done((result) => {
-    console.log(" >>> goBackToWork succes! result: " + result);
-    $(".stop-button .lds-ellipsis").addClass("hidden")
-  })
-  .fail((error) => {
-    console.log(" >>> goBackToWork error!: " + JSON.stringify(error));
-  })
-
-  state.isWorking = true;
-  state.workingSinceTimestamp = Date.now();
-  state.stoppedSinceTimestamp = null;
-  $("section.is-stopped").addClass("hidden");
-  $("section.is-working").removeClass("hidden");
-  getAndShowReasons();
-}
 
 function getAndShowTime() {
   let now = new Date();
-  let dayOfMonth = keepTwoDigits(now.getDate());
-  let month = keepTwoDigits(now.getMonth());
   let year = now.getFullYear();
+  let month = keepTwoDigits(now.getMonth());
+  let dayOfMonth = keepTwoDigits(now.getDate());
   let hours = keepTwoDigits(now.getHours());
   let minutes = keepTwoDigits(now.getMinutes());
   let timeString = dayOfMonth + "/" + month + "/" + year + " " + hours + ":" + minutes;
